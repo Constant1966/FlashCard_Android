cc090647a88dcd122911708be6cbe37f78f84830
https://github.com/Constant1966/FlashCard_Android <constantlorvenson@gmail.com>
2024-03-13 23:17:43 -0400

    My Flash Cards in android

---
+++ app/src/main/java/com/example/flashcardapp/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.flashcardapp
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+//import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+//import androidx.core.view.ViewCompat
+//import androidx.core.view.WindowInsetsCompat
+import android.widget.TextView
+import android.view.View
+import com.example.flashcardapp.R.id.flashcard_question
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val flashcardQuestion = findViewById<TextView>(flashcard_question)
+        val flashcardAnswer = findViewById<TextView>(R.id.flashcard_answer)
+        flashcardQuestion.setOnClickListener {
+            flashcardQuestion.visibility = View.INVISIBLE
+            flashcardAnswer.visibility = View.VISIBLE
+        }
+        flashcardAnswer.setOnClickListener {
+            flashcardQuestion.visibility = View.VISIBLE
+            flashcardAnswer.visibility = View.INVISIBLE
+        }
+    }
+
+
+}
